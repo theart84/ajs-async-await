d2777bfbd163494e7840f8b817d63921f8f4d8da
theart84 <artshadow74@gmail.com>
2021-03-09 18:14:19 +0500

    fixed problems

--- src/js/GameSavedLoader.js
+++ src/js/GameSavedLoader.js
@@ -1,12 +1,13 @@
 import read from './reader';
 import json from './parser';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static async load() {
     try {
-      const data = await read(); // возвращается Promise!
-      const value = await json(data); // возвращается Promise!
-      return value;
+      const data = await read();
+      const value = await json(data);
+      return new GameSaving(value);
     } catch (err) {
       return Promise.reject(err);
     }
